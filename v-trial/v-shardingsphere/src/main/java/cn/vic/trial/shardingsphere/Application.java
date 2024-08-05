da14698b672786d8eea76cd8054d07dea63a68c4
liusheng <sheng.liu@marketin.cn>
2024-08-05 17:27:29 +0800

    optimize: boot package

---
+++ v-trial/v-shardingsphere/src/main/java/cn/vic/trial/shardingsphere/Application.java
@@ -0,0 +1,16 @@
+//package cn.vic.trial.shardingsphere;
+//
+//import org.springframework.boot.SpringApplication;
+//import org.springframework.boot.autoconfigure.SpringBootApplication;
+//
+///**
+// * @author sheng.liu@marketin.cn
+// * @date 2024/8/5
+// */
+//@SpringBootApplication
+//public class Application {
+//    public static void main(String[] args) {
+//        SpringApplication.run(Application.class, args);
+//    }
+//
+//}
